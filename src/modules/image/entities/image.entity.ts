4d123160721810eeeac9ffb15ca447765ee5c851
CyberALi1989 <naserianali@gmail.com>
2025-02-05 00:03:51 +0330

    fix image entity to return valid image src

--- src/modules/image/entities/image.entity.ts
+++ src/modules/image/entities/image.entity.ts
@@ -2,6 +2,7 @@ import {BaseEntity} from "../../../common/abstracts/base.entity";
 import {AfterLoad, Column, Entity, ManyToOne} from "typeorm";
 import {EntityEnum} from "../../../common/enums/entity.enum";
 import {UserEntity} from "../../user/entities/user.entity";
+import * as process from "node:process";
 
 @Entity(EntityEnum.Image)
 export class ImageEntity extends BaseEntity {
@@ -20,6 +21,6 @@ export class ImageEntity extends BaseEntity {
 
   @AfterLoad()
   map() {
-    this.src = `http:localhost:3000/${this.location}`;
+    this.src = `http://localhost:${process.env.PORT}/${this.location}`.replace(/\\+/g, '/');;
   }
 }
